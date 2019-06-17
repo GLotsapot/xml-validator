c66e71d19c38844c4d9365bf2816d4f71712476d
Sawyer Peacock <speacock@shorelinescasinos.com>
2019-06-17 11:43:49 -0400

    Project Related Added some startup and trademark blah blahs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("xmlValidator")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Sawyer Peacock")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
